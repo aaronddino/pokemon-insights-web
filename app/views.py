552c0f8747cbc6db11034c3003cce6e64442bd0d
Aarond (Home) MacBook Pro <aaronddino@gmail.com>
2020-08-30 21:21:11 +1000

    Finally got images extracted for all pokemon using pokemon fandom api using thumbnail. Using same api to extract basic physiology summary to display for description

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from django.utils.safestring import mark_safe
 from django.views.generic import TemplateView
-from pokedex.api import get_pokemon_data, get_wikipedia_summary, get_wikipedia_image
+from pokedex.api import get_pokemon_data, get_pokemon_wiki_image, get_pokemon_wiki_summary
 
 
 def home(request):
@@ -10,15 +11,15 @@ def home(request):
         context['pokemon_name'] = search_input.capitalize()
         try:
             searched_pokemon_data = get_pokemon_data(str_pokemon_name=search_input)
-            pokemon_wiki_summary = get_wikipedia_summary(search_keywords=search_input)
-            pokemon_wiki_image = get_wikipedia_image(search_keywords=search_input)
+            pokemon_wiki_summary = mark_safe(get_pokemon_wiki_summary(str_pokemon_name=search_input)) #get_wikipedia_summary(search_keywords=search_input)
+            pokemon_wiki_image = get_pokemon_wiki_image(str_pokemon_name=search_input) #get_wikipedia_image(search_keywords=search_input)
             dex_id = str(searched_pokemon_data['id'])
             while len(dex_id) < 3:
                 dex_id = '0' + dex_id
             else:
                 dex_id
             context['id'] = dex_id
-            context['type'] = '/'.join([pokemon['type']['name'] for pokemon in searched_pokemon_data['types']])
+            context['type'] = '/'.join([pokemon['type']['name'].capitalize() for pokemon in searched_pokemon_data['types']])
             context['summary'] = pokemon_wiki_summary
             context['image'] = pokemon_wiki_image
         except Exception as e:
--- pokedex/api.py
+++ pokedex/api.py
@@ -24,7 +24,32 @@ def get_wikipedia_summary(search_keywords):
     return summary_data
 
 
+# Unused due to inability to get all pokemon from wikipedia e.g ivysaur
 def get_wikipedia_image(search_keywords):
     response = requests.get('https://en.wikipedia.org/api/rest_v1/page/summary/{}'.format(search_keywords))
     image_link = response.json()['originalimage']['source']
     return image_link
+
+
+def get_pokemon_wiki_page(str_pokemon_name):
+    response = requests.get('https://pokemon.fandom.com/api.php?action=query&titles={}&format=json'.format(str_pokemon_name))
+    page = next(iter(response.json()['query']['pages']))
+
+    return int(page)
+
+
+def get_pokemon_wiki_image(str_pokemon_name):
+    page_id = get_pokemon_wiki_page(str_pokemon_name)
+    response = requests.get('https://pokemon.fandom.com/api/v1/Articles/Details?ids={}'.format(page_id))
+    image_url = response.json()['items'][str(page_id)]['thumbnail']
+
+    return image_url
+
+
+def get_pokemon_wiki_summary(str_pokemon_name):
+    page_id = get_pokemon_wiki_page(str_pokemon_name)
+    response = requests.get('https://pokemon.fandom.com/api/v1/Articles/AsSimpleJson?id={}'.format(page_id))
+    ls_physiology_text = [text['content'][0]['text'] for text in response.json()['sections'] if text['title'] in ['Physiology', 'Characteristics', 'Behavior']]
+    physiology_text = '<br><br>'.join(ls_physiology_text)
+    return physiology_text
+    
